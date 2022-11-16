2e08387c974353de9e476a9c2af42d68e4bcd440
gorzek-ryan <ryan.gorzek@gmail.com>
2022-11-15 18:37:08 -0800

    Add tuning kernel plotting for all cells and each plane

--- tuning/rand_ori_sf.m
+++ tuning/rand_ori_sf.m
@@ -1,6 +1,7 @@
 
 %% Preprocessing
 
+clearvars; close all;
 file_path = "/Users/ryan.gorzek/Library/CloudStorage/GoogleDrive-ryan.gorzek@gmail.com/Shared drives/Astrocytes - Protocadherins/";
 addpath(genpath(fullfile(file_path)));
 
@@ -56,4 +57,107 @@ save(strcat(file_path, "randorisf_cells_tbl.mat"), "cells_tbl");
 
 %% Plotting
 
-% Plot kernels for each eye.
+clearvars; close all;
+file_path = "/Users/ryan.gorzek/Library/CloudStorage/GoogleDrive-ryan.gorzek@gmail.com/Shared drives/Astrocytes - Protocadherins/";
+cells_tbl = load(strcat(file_path, "randorisf_cells_tbl.mat")).cells_tbl;
+
+subj_ids = ["john01", "john02", "john03"]; % 
+genotypes = ["KO", "KO", "Het"];
+plane_ids = ["000", "001", "002"]; % 
+for subj = subj_ids
+    % First, plot tuning kernels for each eye from all planes in each animal.
+    subj_idx = cells_tbl.subject_id == subj;
+    subj_tbl = cells_tbl(subj_idx, :);
+    title = sprintf("%s (%s) %i cells", subj, genotypes(subj_ids == subj), size(subj_tbl, 1));
+    save_path = strcat(file_path, "plots", filesep, subj, "_all_kernels.jpeg");
+    kernPlot(subj_tbl, num_rows=15, title=title, save_path=save_path);
+    %
+    for plane = plane_ids
+        % Then, plot tuning kernels for each eye from each plane.
+        plane_idx = subj_idx & cells_tbl.plane_id == plane;
+        plane_tbl = cells_tbl(plane_idx, :);
+        title = sprintf("%s_%s (%s) %i cells", subj, plane, genotypes(subj_ids == subj), size(plane_tbl, 1));
+        save_path = strcat(file_path, "plots", filesep, subj, "_", plane, "_kernels.jpeg");
+        kernPlot(plane_tbl, title=title, save_path=save_path);
+        %
+    end
+end
+
+% Define plotting functions.
+
+function kernPlot(cells_table, NVAs)
+% KERNPLOT Plot orisf tuning kernels from a table of sbxorisf_new data.
+arguments
+    cells_table
+    NVAs.num_rows (1,1) double = 10
+    NVAs.num_cols (1,1) double = NaN
+    NVAs.title string = ""
+    NVAs.save_path string = ""
+end
+
+num_cells = size(cells_table, 1);
+if isnan(NVAs.num_cols)
+    NVAs.num_cols = ceil(num_cells/NVAs.num_rows);
+end
+% Create a 5x10 tile area for each cell, spacing columns by 1 tile.
+f = figure; hold on; set(gcf, "Color","w");
+t = tiledlayout(NVAs.num_rows*5, ...
+                NVAs.num_cols*10, ...
+                "TileSpacing","tight", ...
+                "Padding","compact");
+for cell_num = 1:num_cells
+    % Concatenate ipsi and contra side-by-side, separated by a NaN column.
+    kern_contra = cells_table{cell_num, "kern_contra"}{:};
+    kern_ipsi = cells_table{cell_num, "kern_ipsi"}{:};
+    kerns = horzcat(kern_ipsi, nan(size(kern_contra,1), 1), kern_contra);
+    % Plot.
+    nexttile([5,10]);
+    imagesc(kerns, "AlphaData",~isnan(kerns));
+    axis tight; axis off; pbaspect([2.04,1,1]);
+    if NVAs.num_rows == 10 && ...
+       cell_num <= NVAs.num_cols
+        title("  I              C ");
+    elseif cell_num <= NVAs.num_cols
+        title(" I       C");
+    end
+end
+% Dynamically resize the figure such that the kernel plots appear similar
+% across animals/planes.
+if NVAs.num_rows == 10
+    f.Position = [-231, 1130, 125*NVAs.num_cols, 70*NVAs.num_rows];
+else
+    f.Position = [-231, 1130, 78*NVAs.num_cols, 50*NVAs.num_rows];
+end
+% Set title and axis labels.
+title(t, NVAs.title, ...
+         "Interpreter", "none", ...
+         "FontWeight", "bold", ...
+         "FontSize", 15);
+annotation("textbox", ...
+           "String","Orientation", ...
+           "FontWeight","bold", ...
+           "FontSize",15, ...
+           "EdgeColor","none", ...
+           "HorizontalAlignment","center", ...
+           "VerticalAlignment","middle", ...
+           "Rotation",90, ...
+           "Position",[0.02, 0.5, 0, 0]);
+annotation("textbox", ...
+           "String","Spatial Frequency", ...
+           "FontWeight","bold", ...
+           "FontSize",15, ...
+           "EdgeColor","none", ...
+           "HorizontalAlignment","center", ...
+           "VerticalAlignment","middle", ...
+           "Rotation",0, ...
+           "Position",[0.4, 0.025, 0.2, 0]);
+% Set one y-axis and one x-axis per plot for reference.
+
+% Save the figure to file.
+imwrite(getframe(f).cdata, ...
+        NVAs.save_path, ...
+        "jpeg", ...
+        "Mode", "lossless", ...
+        "Quality", 100);
+
+end
